fd8d1ef20939a21cff4adae18bfc2cba50d2165d
tanishq20 <66270248+tanishq20@users.noreply.github.com>
2021-03-05 23:02:07 +0530

    Update Javascipt.js

--- Javascipt.js
+++ Javascipt.js
@@ -20,22 +20,6 @@ $(document).ready(function () {
   });
 });
 
-///////////////// fixed menu on scroll for desktop
-if ($(window).width() > 992) {
-  $(window).scroll(function(){  
-     if ($(this).scrollTop() > 40) {
-        $('#navbar_top').addClass("fixed-top");
-        // add padding top to show content behind navbar
-        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
-        // document.getElementsByClassName('navbar').style.backgroundColor = "red";
-        // $('body').css('backgroundColor', $('.navbar') + 'red');
-      }else{
-        $('#navbar_top').removeClass("fixed-top");
-         // remove padding top from body
-        $('body').css('padding-top', '0');
-      }   
-  });
-} // end if
 
 
 
